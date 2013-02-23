22ce991783bd283b05e1e4ba7f7d18fe5d57511d
Jan Seiffert <kaffeemonster@googlemail.com>
2013-02-23 23:39:07 +0100

    move defines
    the USB_SERIAL_INTERNAL is a give away. Other code has no buisiness
    with it, move it to the .c to confine it there.

--- firmware/usb_serial.c
+++ firmware/usb_serial.c
@@ -5,6 +5,8 @@
  * This is NOT the origignal version of the original code.
  * If you are interrested in it, visit the above mentioned web site.
  *
+ * Copyright (c) 2013 Jan Seiffert, Frederic Endner-Duehr, Dirk Raufer
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -55,13 +57,11 @@
 // Version 1.7: fix usb_serial_set_control
 
 #include <stdbool.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
 
-#define USB_SERIAL_PRIVATE_INCLUDE
 #include "usb_serial.h"
-
-#ifndef EPINT_D0
-#define EPINT_D0 0
-#endif
 
 /**************************************************************************
  *
@@ -135,6 +135,78 @@
 #define SUPPORT_ENDPOINT_HALT
 
 
+/**************************************************************************
+ *
+ *  Internal defines
+ *
+ **************************************************************************/
+
+#ifndef EPINT_D0
+# define EPINT_D0 0
+#endif
+
+#define EP_TYPE_CONTROL             0x00
+#define EP_TYPE_BULK_IN             0x81
+#define EP_TYPE_BULK_OUT            0x80
+#define EP_TYPE_INTERRUPT_IN        0xC1
+#define EP_TYPE_INTERRUPT_OUT       0xC0
+#define EP_TYPE_ISOCHRONOUS_IN      0x41
+#define EP_TYPE_ISOCHRONOUS_OUT     0x40
+#define EP_SINGLE_BUFFER            0x02
+#define EP_DOUBLE_BUFFER            0x06
+#define EP_SIZE(s)  ((s) == 64 ? 0x30 : \
+                    ((s) == 32 ? 0x20 : \
+                    ((s) == 16 ? 0x10 : \
+                                 0x00)))
+
+#define MAX_ENDPOINT                4
+
+#define LSB(n) (n & 255)
+#define MSB(n) ((n >> 8) & 255)
+
+#if defined(__AVR_AT90USB162__)
+# define HW_CONFIG()
+# define PLL_CONFIG() (PLLCSR = (_BV(PLLE)|_BV(PLLP0)))
+# define USB_CONFIG() (USBCON = _BV(USBE))
+# define USB_FREEZE() (USBCON = (_BV(USBE)|_BV(FRZCLK)))
+#elif defined(__AVR_ATmega32U4__)
+# define HW_CONFIG()  (UHWCON = 0x01)
+# define PLL_CONFIG() (PLLCSR = 0x12)
+# define USB_CONFIG() (USBCON = (_BV(USBE)|_BV(OTGPADE)))
+# define USB_FREEZE() (USBCON = (_BV(USBE)|_BV(FRZCLK)))
+#elif defined(__AVR_AT90USB646__)
+# define HW_CONFIG()  (UHWCON = 0x81)
+# define PLL_CONFIG() (PLLCSR = 0x1A)
+# define USB_CONFIG() (USBCON = (_BV(USBE)|_BV(OTGPADE)))
+# define USB_FREEZE() (USBCON = (_BV(USBE)|_BV(FRZCLK)))
+#elif defined(__AVR_AT90USB1286__)
+# define HW_CONFIG()  (UHWCON = 0x81)
+# define PLL_CONFIG() (PLLCSR = 0x16)
+# define USB_CONFIG() (USBCON = (_BV(USBE)|_BV(OTGPADE)))
+# define USB_FREEZE() (USBCON = (_BV(USBE)|_BV(FRZCLK)))
+#endif
+
+/* standard control endpoint request types */
+#define GET_STATUS                  0
+#define CLEAR_FEATURE               1
+#define SET_FEATURE                 3
+#define SET_ADDRESS                 5
+#define GET_DESCRIPTOR              6
+#define GET_CONFIGURATION           8
+#define SET_CONFIGURATION           9
+#define GET_INTERFACE               10
+#define SET_INTERFACE               11
+/* HID (human interface device) */
+#define HID_GET_REPORT              1
+#define HID_GET_PROTOCOL            3
+#define HID_SET_REPORT              9
+#define HID_SET_IDLE                10
+#define HID_SET_PROTOCOL            11
+/* CDC (communication class device) */
+#define CDC_SET_LINE_CODING         0x20
+#define CDC_GET_LINE_CODING         0x21
+#define CDC_SET_CONTROL_LINE_STATE  0x22
+
 
 /**************************************************************************
  *
@@ -153,19 +225,19 @@
 #define CDC_RX_ENDPOINT     3
 #define CDC_TX_ENDPOINT     4
 #if defined(__AVR_AT90USB162__)
-#define CDC_ACM_SIZE        16
-#define CDC_ACM_BUFFER      EP_SINGLE_BUFFER
-#define CDC_RX_SIZE         32
-#define CDC_RX_BUFFER       EP_DOUBLE_BUFFER
-#define CDC_TX_SIZE         32
-#define CDC_TX_BUFFER       EP_DOUBLE_BUFFER
+# define CDC_ACM_SIZE        16
+# define CDC_ACM_BUFFER      EP_SINGLE_BUFFER
+# define CDC_RX_SIZE         32
+# define CDC_RX_BUFFER       EP_DOUBLE_BUFFER
+# define CDC_TX_SIZE         32
+# define CDC_TX_BUFFER       EP_DOUBLE_BUFFER
 #else
-#define CDC_ACM_SIZE        16
-#define CDC_ACM_BUFFER      EP_SINGLE_BUFFER
-#define CDC_RX_SIZE         64
-#define CDC_RX_BUFFER       EP_DOUBLE_BUFFER
-#define CDC_TX_SIZE         64
-#define CDC_TX_BUFFER       EP_DOUBLE_BUFFER
+# define CDC_ACM_SIZE        16
+# define CDC_ACM_BUFFER      EP_SINGLE_BUFFER
+# define CDC_RX_SIZE         64
+# define CDC_RX_BUFFER       EP_DOUBLE_BUFFER
+# define CDC_TX_SIZE         64
+# define CDC_TX_BUFFER       EP_DOUBLE_BUFFER
 #endif
 
 static const uint8_t PROGMEM endpoint_config_table[] = {
